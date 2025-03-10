228d0bab39ce908855ccfc846bf6654a9082a79c
salma-salsabila <poppy31y14@gmail.com>
2025-03-10 12:22:11 +0700

    Initial Commit

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,24 @@
+<html>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <body>
+        <div class="container mt-4">
+            <div class="card">
+                <div class="card-header">
+                    <h1>Profile Saya</h1>
+                </div>
+                <div class="card-body">
+                    <img src="profile.jpg" alt="Profile Picture" width="150">
+                    <p>Salma Salsabila</p>
+                    <p>Email: poppy31y14@gmail.com</p>
+                    <p>Bio: Take a chance with me</p>
+                    <h5><p>Skills:</p></h5>
+                    <ul>
+                        <li>Mukbang</li>
+                        <li>Masak</li>
+                        <li>Healing</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
